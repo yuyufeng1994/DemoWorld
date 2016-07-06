efed09633442179b62873f3f446ade5bd10b1007
Yu Yufeng <Yu Yufeng@yyf>
2016-07-06 11:46:58 +0800

    HTMLTOPDF

--- src/yyf/utils/office/itext/HTMlToPDF.java
+++ src/yyf/utils/office/itext/HTMlToPDF.java
@@ -1,25 +1,21 @@
 package yyf.utils.office.itext;
 
 import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.StringBufferInputStream;
-import java.io.StringReader;
-import java.io.StringWriter;
 import java.nio.charset.Charset;
-
-import javax.swing.text.rtf.RTFEditorKit;
 
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.pdf.PdfWriter;
 import com.itextpdf.tool.xml.XMLWorkerHelper;
-
-public class App {
+/**
+ * html代码转化为pdf
+ * @author Yu Yufeng
+ *
+ */
+public class HTMlToPDF {
 	public static void main(String[] args) throws DocumentException, IOException {
 		// step 1
 		Document document = new Document();
@@ -27,11 +23,11 @@ public class App {
 		PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream("i://a.pdf"));
 		// step 3
 		document.open();
-//		document.
+		// document.
 		// step 4
-		String s = "<img width='500' src='http://localhost:8080/sok/doc/download-file/480/jpg'/><br><img width='500' src='http://localhost:8080/sok/doc/download-file/479/png'/><br>";
+		String s = "<h1>测试</h1>";
 		InputStream in = new ByteArrayInputStream(s.getBytes());
-//		in = new FileInputStream("i://a.html");
+		// in = new FileInputStream("i://a.html");
 		XMLWorkerHelper.getInstance().parseXHtml(writer, document, in, Charset.forName("UTF-8"));
 		// step 5
 		document.close();
@@ -41,7 +37,6 @@ public class App {
 	public static InputStream stringToInputStream(String str) {
 		try {
 			InputStream is = new ByteArrayInputStream(str.getBytes());
-
 			int byteRead;
 			while ((byteRead = is.read()) != -1) {
 				System.out.print((char) byteRead);
@@ -49,7 +44,6 @@ public class App {
 			is.close();
 			return is;
 		} catch (Exception e) {
-
 			return null;
 		}
 
