dbce36ae06c684e06efce5210b476d65f81f5ef4
Yu Yufeng <Yu Yufeng@yyf>
2016-10-25 14:49:16 +0800

    状态模式

---
+++ src/yyf/designpatterns/state/ConcreteStateA.java
@@ -0,0 +1,16 @@
+package yyf.designpatterns.state;
+
+/**
+ * 具体状态类
+ * @author Yu Yufeng
+ *
+ */
+public class ConcreteStateA implements State {
+
+	@Override
+	public void handle(String sampleParameter) {
+
+		System.out.println("ConcreteStateA handle ：" + sampleParameter);
+	}
+
+}
