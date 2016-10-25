6901be3f3b9523743a48d27b27fd71bf03875edb
Yu Yufeng <Yu Yufeng@yyf>
2016-10-25 08:45:47 +0800

    修改

--- src/yyf/designpatterns/proxy/ProxyHandler.java
+++ src/yyf/designpatterns/proxy/ProxyHandler.java
@@ -17,7 +17,7 @@ public class ProxyHandler {
 
 	public String invoke(Object proxy, Method method, Object[] args) throws Throwable {
 		// 在转调具体目标对象之前，可以执行一些功能处理
-		System.out.println("after");
+		System.out.println("before");
 		// 转调具体目标对象的方法
 		method.invoke(proxied, args);
 		// 在转调具体目标对象之后，可以执行一些功能处理
