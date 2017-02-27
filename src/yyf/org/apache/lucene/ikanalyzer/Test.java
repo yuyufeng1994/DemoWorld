56d2e03723909664b7d4a33c71e1329f881d4cde
yyf <416530244@qq.com>
2017-02-27 11:31:11 +0800

    ik分词器

--- src/yyf/org/apache/lucene/ikanalyzer/Test.java
+++ src/yyf/org/apache/lucene/ikanalyzer/Test.java
@@ -12,14 +12,16 @@ import org.wltea.analyzer.lucene.IKAnalyzer;
 
 public class Test {
 	public static void main(String[] args) {
+		String query="巴巴快巴是互联网+道路客运公司";
+		
 		// 构建IK分词器，使用smart分词模式
 		Analyzer analyzer = new IKAnalyzer(true);
-
 		// 获取Lucene的TokenStream对象
 		TokenStream ts = null;
 		try {
 			ts = analyzer.tokenStream("myfield",
-					new StringReader("这是一个中文分词的例子，你可以直接运行它！IKAnalyer can analysis english text too"));
+					new StringReader(query));
+
 			// 获取词元位置属性
 			OffsetAttribute offset = ts.addAttribute(OffsetAttribute.class);
 			// 获取词元文本属性
@@ -31,13 +33,14 @@ public class Test {
 			ts.reset();
 			// 迭代获取分词结果
 			while (ts.incrementToken()) {
-				System.out.println(offset.startOffset() + " - " + offset.endOffset() + " : " + term.toString() + " | "
-						+ type.type());
+				// System.out.println(offset.startOffset() + " - " +
+				// offset.endOffset() + " : " + term.toString() + " | "+
+				// type.type());
+				System.out.print(term.toString() + " | ");
 			}
 			// 关闭TokenStream（关闭StringReader）
 			ts.end(); // Perform end-of-stream operations, e.g. set the final
 						// offset.
-
 		} catch (IOException e) {
 			e.printStackTrace();
 		} finally {
